ad19a66fdc9c2858259750ef2f6b188808f30239
LUXIANZE <luxianze98@gmail.com>
2020-10-18 16:14:30 +0800

    Set Up Forwarding of receipt to intended receiver

--- README.md
+++ README.md
@@ -12,3 +12,7 @@ This project is aimed to provide a service of generating pdf receipt and send to
 1. Open up terminal (ctrl + `)
 2. To install project dependencies, type -> **npm i** 
 3. To Start pproject with Nodemon, type -> **npx nodemon index.js**
+
+## Projec Components Intro
+- The **Public** folder consists of static files that are intended to be served publicly.
+- The Receipt will be generated as html email to be sent to intended receiver
--- index.js
+++ index.js
@@ -1,22 +1,46 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const fs = require('fs');
+const cors = require('cors')
+const { json } = require('express');
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'example@gmail.com',
+    pass: 'examplepassword'
+  }
+});
 
 const app = express()
+app.use(cors())
 app.use(express.json())
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = 3000
+// const startCPU = process.cpuUsage()
 
 app.get('/', (req, res) => {
-  res.send('App Alive')
+    const uptime = process.uptime()
+    const cpuUsage = process.cpuUsage()
+    const alliveMessage = `<h1>System Alive</h1>`
+    const appUsage = `<p><h3>Up Time</h3></p> <ul><li>Up Time : ${uptime} seconds</li></ul> <p><h3>CPU Usage</h3></p> <ul><li>User: ${cpuUsage.user}</li><li>System: ${cpuUsage.system}</li></ul>`
+    res.type('pdf')
+    res.send(alliveMessage + appUsage)
 })
 
 app.post('/Receipt', (req, res) => {
+    /**
+     * Logic for pdf Receipt Generator:
+     * - Load Template html locally
+     * - Replace necessary info
+     * - Email to destination with the updated template email (with transaction data)
+     **/
     const { paidBy, receiptDate, amount, receivedBy } = req.body;
-    console.log(req.body)
 
-    const data = fs.readFileSync('./receiptTemplate.html', {encoding:'utf8', flag:'r'});
+    const data = fs.readFileSync('./receiptTemplate.html', {encoding:'utf8', flag:'r'})
     
     let template = data
 
@@ -24,8 +48,22 @@ app.post('/Receipt', (req, res) => {
     template = template.replace('receiptDate', `${receiptDate}`)
     template = template.replace('amount', `${amount}`)
     template = template.replace('receivedBy', `${receivedBy}`)
-    console.log(`\nafter replacing \n ${template}`)
-    
+
+    var mailOptions = {
+        from: 'sender@gmail.com',
+        to: 'receiver@siswa.um.edu.my',
+        subject: 'Sending Email using Node.js',
+        html: `${template}`
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+    });
+
     res.send(template)
 
 })
